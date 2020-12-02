4108af85e793665c24fc7c758cba79b164bfebaa
Anton Gushcha <ncrashed@protonmail.com>
2020-12-02 22:20:14 +0300

    Workaround for bug https://github.com/ucsd-progsys/liquidhaskell/issues/1807

--- nix/haskell/liquid-src.nix
+++ nix/haskell/liquid-src.nix
@@ -1,6 +1,6 @@
 {
   url = "https://github.com/ucsd-progsys/liquidhaskell.git";
-  sha256 = "1dz71474xm2b5h32hlvn7kcga5msidkgqhxw2x34myl9fr8qqlix";
-  rev = "bc39f4bde76b58a61ecb3b2adbba3868955157cc";
+  sha256 = "00wfc52h65yxb6cadvs0ygp9mf2si5020xgzlhsqjr55b4yahkl3";
+  rev = "677df28b0e563a2163dafe204d3f22e339c28aa3";
   fetchSubmodules = true;
 }
