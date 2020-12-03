3db7550cf23f85fee12380fcf49b794375b97881
Anton Gushcha <ncrashed@protonmail.com>
2020-12-03 04:36:23 +0300

    Add liquidhaskell-nix overlay

--- nix/release.nix
+++ nix/release.nix
@@ -1,23 +1,14 @@
 let
   nixpkgs = import ./pkgs.nix;
   project = import (import ./project.nix) { inherit nixpkgs; };
-in project {
+in project rec {
   packages = {
     liquidhaskell-test = ../.;
   };
-  shellTools = pkgs: with pkgs.haskellPackages; [
-    cabal-install
-    ghcid
-    pkgs.z3
-  ];
   shellHook = pkgs: ''
-    configureFlags=--extra-lib-dirs=${pkgs.z3.lib}/lib # Or liquid plugin won't find z3 executable
+    ${pkgs.addLiquidSolverHook}
   '';
-  overlays = [
-    (import ./haskell/pkgs-overlay.nix)
-  ];
-  overlaysAfter = [
-    (import ./solver.nix)
-  ];
+  overlays = [ (import (import ./liquidhaskell.nix) { inherit compiler; }) ];
+  overlaysAfter = [ (import ./solver.nix) ];
   compiler = "ghc8102";
 }
--- nix/solver.nix
+++ nix/solver.nix
@@ -1,11 +1,8 @@
 self: super:
 let
   ghc-override = import "${import ./project.nix}/lib/ghc-override.nix";
-  lib  = self.haskell.lib;
-  addSystemLibs = drv: xs: lib.overrideCabal drv (drv: { librarySystemDepends = (drv.librarySystemDepends or []) ++ xs; });
-  addSolver = drv: addSystemLibs drv [self.z3];
-  dontHaddock = lib.dontHaddock;
+  pkgs = self;
 in
 ghc-override "ghc8102" ( self: super: {
-  liquidhaskell-test = addSolver super.liquidhaskell-test;
+  liquidhaskell-test = pkgs.addLiquidSolver super.liquidhaskell-test;
 }) super
